04e6aca4a569b828a94b33545cd1a32f07e15f05
Paul Brousseau <object88@gmail.com>
2017-03-21 22:00:50 -0600

    First attempt at specifying priority (#9)

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import path from 'path';
 
 type Options = {
   filename: string,
-  path: string,
   includeMap?: boolean,
+  path: string,
+  priorities?: Array<string>,
 };
 
 type Result = {
@@ -13,22 +14,42 @@ type Result = {
 };
 
 type ResultEntry = {
-  source: string,
   map?: string,
+  priority?: number,
+  source: string,
 };
 
 function WebpackFileList(options: Options) {
   if (!options.filename) {
     throw new Error("filename property is required on options");
   } else if (!options.path) {
-    throw new Error("filename property is required on options");
+    throw new Error("path property is required on options");
   }
 
   this.options = options;
 }
 
+WebpackFileList.prototype.applyPriorities = function(result: Result) {
+  if (!this.options.priorities) {
+    return;
+  }
+
+  let priorityCount = 0;
+  this.options.priorities.forEach((p) => {
+    const resultEntry = result[p];
+    if (!resultEntry) {
+      return;
+    }
+
+    resultEntry.priority = priorityCount;
+    priorityCount++;
+  });
+}
+
 WebpackFileList.prototype.apply = function(compiler) {
   compiler.plugin('emit', (compilation, callback) => {
+    const priorities = this.options.priorities || [];
+
     const json: Result = {};
     compilation.chunks.forEach((chunk) => {
       chunk.files.forEach((filename) => {
@@ -46,12 +67,15 @@ WebpackFileList.prototype.apply = function(compiler) {
       });
     });
 
+    // Apply priorities
+    this.applyPriorities(json);
+
     // Write out JSON
     const destination = path.join(this.options.path, this.options.filename);
     const blob = JSON.stringify(json, undefined, 2);
     const buffer = new Buffer(blob);
 
-    const mode = 0;
+    const mode = 0x1a4;
     fs.open(destination, 'w', mode, (openErr, fd) => {
       if (openErr) {
         console.error(`Failed to open file '${destination}' with error '${openErr.toString()}'; quitting.`);
